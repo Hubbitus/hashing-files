ea11e9f8d99e67bc25bb638179e2b40ed07e0274
Pavel Alexeev <pahan@hubbitus.info>
2019-01-10 20:44:16 +0300

    Work with postgres DB

--- enumerate.files.bash
+++ enumerate.files.bash
@@ -6,27 +6,29 @@ set -o pipefail
 shopt -s expand_aliases
 
 ### Init table
-#alias sql='mysql -u filedupes_u --password=HVYAwNtIe9 filedupes'
-#alias sql='sudo docker exec -i hashingdb_db_1 mysql -u filedupes_u --password=HVYAwNtIe9 -h 127.0.0.1 filedupes'
-alias sql='mysql -u filedupes_u --password=HVYAwNtIe9 -h 127.0.0.1 filedupes'
+# Access without password assumed! Password must be stored in ~/.pgpass
+alias sql='psql -qX1n -v ON_ERROR_STOP=1 -U filedupes_u -h 127.0.0.1 --dbname filedupes'
 sql < enumerate.sql
 
-#DIR=~
-#DIR=/mnt/data/BACKUP.mirror.store
-DIR=/mnt/data/BACKUP.mirror.store/bela.note.vera/bela/u-Pashy/ТРЕНИНГИ/общение/спор/
-#DIR=/mnt/data/BACKUP.mirror.store/bela.note.vera/bela/u-Pashy/ТРЕНИНГИ/общение/спор/argument_files
+DIR=/mnt/data/BACKUP.mirror.store
+
+# Files in incorrect encoding:
+#DIR=/mnt/data/BACKUP.mirror.store/bela.note.vera/bela/u-Pashy/ТРЕНИНГИ/общение/спор/
+# File with ' in name
+#DIR=/mnt/data/BACKUP.mirror.store/bela.note.vera/bela/Music/Enigma
+
+# md5sum chash with \ (slash). See https://unix.stackexchange.com/questions/313733/various-checksums-utilities-precede-hash-with-backslash
+#DIR=/mnt/data/BACKUP.mirror.store/bela.note.vera/bela/.local/share/gvfs-metadata/
 
 FILES_TOTAL=$( find $DIR -type f | pv -l | wc -l )
 echo FILES_TOTAL=$FILES_TOTAL
-
-#FILES_TOTAL=435902
 
 SQL_INSERT_PATTERN="INSERT INTO files (dir, filename, inode, size, md5, crc32, xxhash) VALUES('%s', '%s', %d, %d, '%s', '%s', '%s')"
 
 time {
 # https://unix.stackexchange.com/questions/39623/trap-err-and-echoing-the-error-line
 err_report() {
-	echo "Error [$? - $2] on line $1 [$(caller)]" >&2
+	echo "Error [$?] on [$(caller)]" >&2
 	echo "ERROR process file [$F]" >&2
 	# Exit from main script even if error happened in subshell. See https://unix.stackexchange.com/questions/48533/exit-shell-script-from-a-subshell/48542#48542
 	sleep 1 # To see error-messages
@@ -35,16 +37,16 @@ err_report() {
 trap 'err_report $LINENO $?' ERR
 
 function insertString(){
-	printf "$SQL_INSERT_PATTERN" "${1//\'/\'\'}" "${2//\'/\'\'}" "$3" "$4" "$5" "$6" "$7"
+	printf "$SQL_INSERT_PATTERN" "${1//\'/''}" "${2//\'/''}" "$3" "$4" "$5" "$6" "$7"
 }
 
 # Handle all file names by http://stackoverflow.com/a/1120952/307525
 find $DIR -type f -print0 | \
-	pv -0 -lptera -s $FILES_TOTAL | \
+	pv -0 -lpteIra -s $FILES_TOTAL | \
 	while IFS= read -r -d $'\0' F; do
 #		echo "File:$F"
 		{
-		echo $( insertString "$( dirname "$F" )" "$( basename "$F" )" $( stat --format=%i "$F" ) $( stat --format=%s "$F" ) "$(  md5sum "$F" | cut -d' ' -f1 )" "$( crc32 "$F" )" "$( xxhsum -H1 "$F" 2>/dev/null | cut -d' ' -f1 )" ) | sql
+			echo $( insertString "$( dirname "$F" )" "$( basename "$F" )" $( stat --format=%i "$F" ) $( stat --format=%s "$F" ) "$( md5sum < "$F" | cut -d' ' -f1 )" "$( crc32 "$F" )" "$( xxhsum -H1 < "$F" 2>/dev/null | cut -d' ' -f1 )" ) | sql
 		}
 		# &
 
--- enumerate.sql
+++ enumerate.sql
@@ -1,13 +1,29 @@
 
 DROP TABLE IF EXISTS files;
 
+-- Hack by https://stackoverflow.com/questions/5090858/how-do-you-change-the-character-encoding-of-a-postgres-database/5091083#5091083
+-- to allow save filenames which are not valid UTF-8! Otherwise we get error: invalid byte sequence for encoding "UTF8"
+UPDATE pg_database SET ENCODING = pg_char_to_encoding('SQL_ASCII') WHERE datname = 'filedupes';
+
 -- For choosen algorithms see hashing.test
 CREATE TABLE files(
-	dir BLOB NOT NULL,
-	filename BLOB NOT NULL,
+	dir text NOT NULL,
+	filename text NOT NULL,
 	inode BIGINT NOT NULL,
 	size BIGINT NOT NULL,
 	md5 CHAR(32) NOT NULL,
 	crc32 CHAR(8) NOT NULL,
-	xxhash VARCHAR(1024) NOT NULL
-) DEFAULT CHARACTER SET = utf8mb4
+	xxhash CHAR(16) NOT NULL
+);
+
+-- By https://stackoverflow.com/questions/25287639/how-to-identify-if-a-value-in-column-can-be-encoded-to-latin-in-postgres
+CREATE OR REPLACE FUNCTION is_utf8(input text) RETURNS boolean
+LANGUAGE plpgsql IMMUTABLE AS $$
+BEGIN
+	PERFORM convert_to(input, 'utf-8');
+		RETURN true;
+EXCEPTION
+	WHEN OTHERS THEN
+		RETURN false;
+END;
+$$;
